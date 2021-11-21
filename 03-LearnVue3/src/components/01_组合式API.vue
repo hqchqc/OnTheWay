51948065717f8d47a1d732be594b96f94dd9d77f
hqchqc <734986933@qq.com>
2021-11-21 22:40:32 +0800

    feat: 新增Vue3.0

---
+++ 03-LearnVue3/src/components/01_组合式API.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script setup="props" lang="ts">
+let repositories = [];
+export const getUserRepositories = async () => {
+  // repositories = await fetchUserRepositories(props.user);
+}
+</script>
+
+
+
+<style scoped></style>
