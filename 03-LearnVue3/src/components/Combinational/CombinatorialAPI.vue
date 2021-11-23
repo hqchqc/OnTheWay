8ae0b0d3986deb6c0bb4bf5133db393054b7d582
hqchqc <734986933@qq.com>
2021-11-23 23:16:03 +0800

    fix: bug fix

---
+++ 03-LearnVue3/src/components/Combinational/CombinatorialAPI.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 v-for="item in repositories">{{ item.name }}</h1>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from '@vue/reactivity';
+import useUserRepositories from './useUserRepositories/useUserRepositories';
+
+const props = defineProps<{ user: { value: string } }>()
+
+const { user } = toRefs(props)
+
+const [repositories, getUserRepositories] = useUserRepositories(user)
+
+// const searchQuery = ref('')
+// const repositoriesMatchingSearchQuery = computed(() => {
+//   // return repositories.value.filter(
+//   //   repository => repository.name.includes(searchQuery.value)
+//   // )
+// })
+
+</script>
+
+
+
+<style scoped></style>
---
+++ 03-LearnVue3/src/components/Combinational/useUserRepositories/useUserRepositories.ts
@@ -0,0 +1,46 @@
+import { onMounted, ref, watch } from "vue";
+
+type repositories = {
+  id: number;
+  name: string;
+};
+
+const fetchUserRepositories = (username: string) => {
+  if (username === "beanBag") {
+    return [
+      {
+        id: 1,
+        name: "vue",
+      },
+      {
+        id: 2,
+        name: "React",
+      },
+    ];
+  } else {
+    return [
+      {
+        id: 1,
+        name: "typescript",
+      },
+      {
+        id: 2,
+        name: "flutter",
+      },
+    ];
+  }
+};
+
+export default function useUserRepositories(user: { value: string }) {
+  const repositories = ref<repositories[]>([]);
+  const getUserRepositories = async () => {
+    repositories.value = await fetchUserRepositories(user.value);
+  };
+  onMounted(getUserRepositories);
+  watch(user, getUserRepositories);
+
+  return {
+    repositories,
+    getUserRepositories,
+  };
+}
