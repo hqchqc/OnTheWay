9e9e1314e8eee33482603cab353d7731bb14981a
hqchqc <734986933@qq.com>
2022-01-05 21:22:57 +0800

    feat: 阅读JavaScript高级程序设计

--- 04-JavaScript高级程序设计（第四版）/三. 语言基础/01_变量.js
+++ 04-JavaScript高级程序设计（第四版）/三. 语言基础/01_变量.js
@@ -1,3 +1,23 @@
-// var message; // undefined
-var message = "Hi~";
-message = 100; // 合法但是不推荐
+{
+  // var message; // undefined
+  var message = "Hi~";
+  message = 100; // 合法但是不推荐
+}
+{
+  function global() {
+    message = "lalala";
+  }
+  global();
+  console.log(message);
+  var a = 1,
+    b = 2,
+    c = 3;
+}
+
+{
+  function foo() {
+    console.log(a);
+    var a = 3;
+  }
+  foo();
+}
